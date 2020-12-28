218324ca9de176d07fce6726723756d1531117b2
Alexey Andreev <dark-cross@rambler.ru>
2020-12-28 21:12:48 +0300

    RadioVolume

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,13 @@
+package ru.netology;
+
+public class Radio {
+    String name;
+    int maxVolume = 10;
+    int minVolume = 0;
+    int currentVolume;
+    boolean on;
+
+
+}
+
+
---
+++ src/test/java/ru/netology/RadioAdvancedTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class RadioAdvancedTest {
+
+    @Test
+    public void shouldGetandSet() {
+        RadioAdvanced Radio = new RadioAdvanced();
+        String expectedName = "Радио";
+
+        assertNull(Radio.getName());
+        Radio.setName(expectedName);
+        assertEquals(expectedName, Radio.getName());
+    }
+
+    @Test
+    public void shoulCheckChangeVolume() {
+        RadioAdvanced Radio = new RadioAdvanced();
+        String expectedName = "Радио";
+
+        assertEquals(0, Radio.getCurrentVolume());
+        Radio.setCurrentVolume(11);
+        assertEquals(0, Radio.getCurrentVolume());
+    }
+}
