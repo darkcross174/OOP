769ad5b8e04ebadc8c6d4d8de327024c63499e20
Alexey Andreev <dark-cross@rambler.ru>
2020-12-29 21:41:02 +0300

    Radio

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,88 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int maxVolume = 10;
+    private int minVolume = 0;
+    private int maxChannel = 9;
+    private int minChannel = 0;
+    private int currentVolume;
+    private int currentChannel;
+
+   public int getCurrentChannel() {
+        return currentChannel;
+   }
+
+    public void setCurrentChannel(int currentChannel) {
+        if (currentChannel > maxChannel) {
+            return;
+        }
+        if (currentChannel < minChannel) {
+            return;
+        }
+        this.currentChannel = currentChannel;
+    }
+
+
+    public void nextChannel() {
+        int currentChannel = this.getCurrentChannel();
+        if (currentChannel == maxChannel) {
+            this.currentChannel = minChannel;
+            return;
+        }
+        this.currentChannel++;
+    }
+
+    public void prevChannel() {
+        int currentRadioStation = this.getCurrentChannel();
+        if (currentChannel == minChannel) {
+            this.currentChannel = maxChannel;
+            return;
+        }
+        this.currentChannel--;
+    }
+
+    public void inputNumberChannel(int numberChannel) {
+        if (numberChannel > maxChannel) {
+            return;
+        }
+        if (numberChannel < minChannel) {
+            return;
+        }
+        this.currentChannel = numberChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > maxVolume) {
+            return;
+        }
+        if (currentVolume < minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void upVolume() {
+        int currentVolume = this.getCurrentVolume();
+        if (currentVolume >= maxVolume) {
+            return;
+        }
+        this.currentVolume++;
+    }
+
+    public void downVolume() {
+        int currentVolume = this.getCurrentVolume();
+        if (currentVolume <= minVolume) {
+            return;
+        }
+        this.currentVolume--;
+    }
+
+}
+
+
+
+
---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,111 @@
+package ru.netology.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+    Radio radio = new Radio();
+
+
+
+    @Test
+    void shouldNextChannel() {
+        int currentChannel = 9;
+        radio.setCurrentChannel(currentChannel);
+        radio.nextChannel();
+        assertEquals(0, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldNextChannel1() {
+        int currentChannel = 5;
+        radio.setCurrentChannel(currentChannel);
+        radio.nextChannel();
+        assertEquals(6, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldPrevChannel() {
+        int currentChannel = 0;
+        radio.setCurrentChannel(currentChannel);
+        radio.prevChannel();
+        assertEquals(9, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldPrevChannel1() {
+        int currentChannel = 5;
+        radio.setCurrentChannel(currentChannel);
+        radio.prevChannel();
+        assertEquals(4, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldInputNumberChannel() {
+        int numberChannel = 7;
+        int currentChannel = 2;
+        radio.setCurrentChannel(currentChannel);
+        radio.inputNumberChannel(numberChannel);
+        assertEquals(7, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldInputNumberChannel1() {
+        int numberChannel = 10;
+        int currentChannel = 2;
+        radio.setCurrentChannel(currentChannel);
+        radio.inputNumberChannel(numberChannel);
+        assertEquals(2, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldInputNumberChannel2() {
+        int numberChannel = -1;
+        int currentChannel = 2;
+        radio.setCurrentChannel(currentChannel);
+        radio.inputNumberChannel(numberChannel);
+        assertEquals(2, radio.getCurrentChannel());
+    }
+
+    @Test
+    public void shouldUpVolume() {
+        int currentVolume = 10;
+        radio.setCurrentVolume(currentVolume);
+        radio.upVolume();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldUpVolume1() {
+        int currentVolume = 1;
+        radio.setCurrentVolume(currentVolume);
+        radio.upVolume();
+        assertEquals(2, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldDownVolume() {
+        int currentVolume = 5;
+        radio.setCurrentVolume(currentVolume);
+        radio.downVolume();
+        assertEquals(4, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldDownVolume1() {
+        int currentVolume = 0;
+        radio.setCurrentVolume(currentVolume);
+        radio.downVolume();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+
+
+}
+
+
+
+
+
+
+
